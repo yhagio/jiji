2978037318c5eac15dc67a88fe2bfe2ead453319
Yuichi Hagio <yhagio87@gmail.com>
2018-05-20 16:56:47 -0400

    Simple views

---
+++ README.md
@@ -0,0 +1,23 @@
+# Dev setup
+
+* Install Postgres
+
+### Hot reloading
+
+Go hot-reloading, so no need to stop and restart server
+
+```bash
+go get github.com/pilu/fresh
+```
+
+In this app repo, run `fresh`
+
+```bash
+fresh
+```
+
+## Tips
+
+```bash
+go get -u github.com/gorilla/mux # -u flag -> get latest version
+```
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var homeTemplate *template.Template
+var contactTemplate *template.Template
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if err := homeTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if err := contactTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	var err error
+	homeTemplate, err = template.ParseFiles(
+		"views/home.gohtml",
+		"views/layouts/footer.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	contactTemplate, err = template.ParseFiles(
+		"views/contact.gohtml",
+		"views/layouts/footer.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":3000", r)
+}
