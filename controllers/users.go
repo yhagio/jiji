e0a6d971e441a84f29e0581beae0e2d1a5bf4a35
Yuichi Hagio <yhagio87@gmail.com>
2018-05-21 15:10:34 -0400

    WIP Signup post form

--- controllers/users.go
+++ controllers/users.go
@@ -1,12 +1,21 @@
 package controllers
 
 import (
+	"fmt"
 	"jiji/views"
 	"net/http"
+
+	"github.com/gorilla/schema"
 )
 
 type Users struct {
 	NewView *views.View
+}
+
+type SignupForm struct {
+	Username string `schema:"username"`
+	Email    string `schema:"email"`
+	Passowrd string `schema:"password"`
 }
 
 func NewUsers() *Users {
@@ -17,8 +26,20 @@ func NewUsers() *Users {
 
 // GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
-	err := u.NewView.Render(w, nil)
-	if err != nil {
+	if err := u.NewView.Render(w, nil); err != nil {
 		panic(err)
 	}
+}
+
+// POST /signup
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		panic(err)
+	}
+	decoder := schema.NewDecoder()
+	signupForm := SignupForm{}
+	if err := decoder.Decode(&signupForm, r.PostForm); err != nil {
+		panic(err)
+	}
+	fmt.Fprint(w, signupForm)
 }
--- main.go
+++ main.go
@@ -30,16 +30,15 @@ func contact(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	homeView = views.NewView("bootstrap",
-		"views/home.gohtml")
-	contactView = views.NewView("bootstrap",
-		"views/contact.gohtml")
+	homeView = views.NewView("bootstrap", "views/home.gohtml")
+	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 
 	usersController := controllers.NewUsers()
 
 	r := mux.NewRouter()
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/signup", usersController.New)
+	r.HandleFunc("/", home).Methods("GET")
+	r.HandleFunc("/contact", contact).Methods("GET")
+	r.HandleFunc("/signup", usersController.New).Methods("GET")
+	r.HandleFunc("/signup", usersController.Create).Methods("POST")
 	http.ListenAndServe(":3000", r)
 }
