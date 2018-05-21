b23cd79899db9afb907437e606af1919226415a7
Yuichi Hagio <yhagio87@gmail.com>
2018-05-21 10:41:45 -0400

    Signup form UI

--- main.go
+++ main.go
@@ -7,8 +7,11 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var homeView *views.View
-var contactView *views.View
+var (
+	homeView    *views.View
+	contactView *views.View
+	signupView  *views.View
+)
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -26,14 +29,25 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func signup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	err := signupView.Render(w, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 	homeView = views.NewView("bootstrap",
 		"views/home.gohtml")
 	contactView = views.NewView("bootstrap",
 		"views/contact.gohtml")
+	signupView = views.NewView("bootstrap",
+		"views/signup.gohtml")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/signup", signup)
 	http.ListenAndServe(":3000", r)
 }
