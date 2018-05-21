84a59d3b16c85e5c6a1bc57b4bbcc37c0c0bd569
Yuichi Hagio <yhagio87@gmail.com>
2018-05-21 00:15:18 -0400

    Bulma CSS, updated View

--- main.go
+++ main.go
@@ -1,43 +1,38 @@
 package main
 
 import (
-	"html/template"
+	"jiji/views"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-var homeTemplate *template.Template
-var contactTemplate *template.Template
+var homeView *views.View
+var contactView *views.View
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := homeTemplate.Execute(w, nil); err != nil {
+	err := homeView.Template.ExecuteTemplate(w,
+		homeView.Layout, nil)
+	if err != nil {
 		panic(err)
 	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := contactTemplate.Execute(w, nil); err != nil {
+	err := contactView.Template.ExecuteTemplate(w,
+		contactView.Layout, nil)
+	if err != nil {
 		panic(err)
 	}
 }
 
 func main() {
-	var err error
-	homeTemplate, err = template.ParseFiles(
-		"views/home.gohtml",
-		"views/layouts/footer.gohtml")
-	if err != nil {
-		panic(err)
-	}
-	contactTemplate, err = template.ParseFiles(
-		"views/contact.gohtml",
-		"views/layouts/footer.gohtml")
-	if err != nil {
-		panic(err)
-	}
+	homeView = views.NewView("bootstrap",
+		"views/home.gohtml")
+	contactView = views.NewView("bootstrap",
+		"views/contact.gohtml")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
---
+++ views/view.go
@@ -0,0 +1,37 @@
+package views
+
+import (
+	"html/template"
+	"path/filepath"
+)
+
+var (
+	LayoutDir   string = "views/layouts/"
+	TemplateExt string = ".gohtml"
+)
+
+type View struct {
+	Template *template.Template
+	Layout   string
+}
+
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+	return files
+}
+
+func NewView(layout string, files ...string) *View {
+	files = append(files, layoutFiles()...)
+	t, err := template.ParseFiles(files...)
+	if err != nil {
+		panic(err)
+	}
+
+	return &View{
+		Template: t,
+		Layout:   layout,
+	}
+}
