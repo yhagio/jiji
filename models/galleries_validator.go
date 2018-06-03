d9f86262c31103b936899786d33872e281b5c026
Yuichi Hagio <yhagio87@gmail.com>
2018-06-03 11:01:08 -0400

    Gallery Create validation

--- models/galleries_validator.go
+++ models/galleries_validator.go
@@ -3,3 +3,45 @@ package models
 type galleryValidator struct {
 	GalleryDB
 }
+
+func (gv *galleryValidator) Create(gallery *Gallery) error {
+	err := galleryValidationFuncs(gallery,
+		gv.userIDRequired,
+		gv.titleRequired)
+
+	if err != nil {
+		return err
+	}
+
+	return gv.GalleryDB.Create(gallery)
+}
+
+func (gv *galleryValidator) userIDRequired(g *Gallery) error {
+	if g.UserId <= 0 {
+		return ErrUserIDRequired
+	}
+	return nil
+}
+
+func (gv *galleryValidator) titleRequired(g *Gallery) error {
+	if g.Title == "" {
+		return ErrTitleRequired
+	}
+	return nil
+}
+
+///////////////////////////////////////////////////////////
+// Reusable validation functions helper
+///////////////////////////////////////////////////////////
+
+type galleryValidationFunc func(*Gallery) error
+
+func galleryValidationFuncs(gallery *Gallery, funcs ...galleryValidationFunc) error {
+	for _, fn := range funcs {
+		err := fn(gallery)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
